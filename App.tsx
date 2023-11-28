9459fb2cc60530fdd1f695d859bbd345c03979d2
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-11-28 17:59:44 +0300

    bottom tab fixed

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 // App.js
 import React from 'react';
-import AppNavigator from './AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FirstScreen from './screens/FirstScreen';
+import SecondScreen from './screens/SecondScreen';
+import ThirdScreen from './screens/SecondScreen';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStack = () => (
+  <Stack.Navigator initialRouteName="">
+    <Stack.Screen name="First" component={FirstScreen} />
+  </Stack.Navigator>
+);
 
 function App() {
   return (
-    <AppNavigator />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Second" component={SecondScreen} />
+        <Tab.Screen name="Third" component={ThirdScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/ThirdScreen.js
@@ -0,0 +1,13 @@
+// ThirdScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ThirdScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Third Screen</Text>
+    </View>
+  );
+};
+
+export default ThirdScreen;
